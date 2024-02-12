155c9bd8cc9090db699cbc766d8c0a2f01e0dade
Michel Fawaz <Michelfawaz87@gmail.com>
2024-02-12 23:05:42 +0200

    Update logo URLs

--- components/MarqueeLogos.tsx
+++ components/MarqueeLogos.tsx
@@ -4,27 +4,48 @@ import Marquee from "@/components/magicui/marquee";
 const logos = [
     {
         name: "Microsoft",
-        img: "https://cdn.simpleicons.org/microsoft/000/fff",
+        img: "https://cdn.simpleicons.org/microsoft/fff",
     },
     {
         name: "Apple",
-        img: "https://cdn.simpleicons.org/apple/000/fff",
+        img: "https://cdn.simpleicons.org/apple/fff",
     },
     {
         name: "Google",
-        img: "https://cdn.simpleicons.org/google/000/fff",
+        img: "https://cdn.simpleicons.org/google/fff",
     },
     {
         name: "Facebook",
-        img: "https://cdn.simpleicons.org/facebook/000/fff",
+        img: "https://cdn.simpleicons.org/facebook/fff",
     },
     {
         name: "LinkedIn",
-        img: "https://cdn.simpleicons.org/linkedin/000/fff",
+        img: "https://cdn.simpleicons.org/linkedin/fff",
     },
     {
         name: "Twitter",
-        img: "https://cdn.simpleicons.org/twitter/000/fff",
+        img: "https://cdn.simpleicons.org/twitter/fff",
+    }
+    ,
+    {
+        name: "Apple",
+        img: "https://cdn.simpleicons.org/apple/fff",
+    },
+    {
+        name: "Google",
+        img: "https://cdn.simpleicons.org/google/fff",
+    },
+    {
+        name: "Facebook",
+        img: "https://cdn.simpleicons.org/facebook/fff",
+    },
+    {
+        name: "LinkedIn",
+        img: "https://cdn.simpleicons.org/linkedin/fff",
+    },
+    {
+        name: "Twitter",
+        img: "https://cdn.simpleicons.org/twitter/fff",
     },
 ];
 
@@ -39,12 +60,12 @@ const Logo = ({ name, img }: { name: string; img: string }) => {
 const MarqueeLogos = () => {
     return (
         <div className="relative flex flex-col items-center justify-center w-full h-full gap-4 py-20 overflow-hidden rounded-lg md:shadow-2xl ">
-            <Marquee  pauseOnHover={true}  className="[--gap:3rem]">
+            <Marquee pauseOnHover={true} className="[--gap:3rem]">
                 {logos.map((logo, idx) => (
                     <Logo key={idx} {...logo} />
                 ))}
             </Marquee>
-           
+
         </div>
     );
 };
